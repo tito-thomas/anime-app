258cdb630f932a87fabc98b977cb46f4b3e019f4
Oluwatito Thomas <thomasoo@cardiff.ac.uk>
2023-02-17 23:12:37 +0000

    Added "generation" column and "experience" columns

--- recommendation.py
+++ recommendation.py
@@ -6,3 +6,11 @@ users = pd.read_csv(r"D:\dataset\users_cleaned.csv")
 anime_lists = pd.read_csv(r"D:\dataset\animelists_cleaned.csv")
 
 users.head()
+
+#uf["birth_date"] = datetime.now() - uf["birth_date"]
+#def date_to_age(days):
+#    seconds = days.total_seconds()
+#    years = seconds/3600/24/365.25
+#    return int(years)
+
+#age = uf["birth_date"].apply(to_age)
