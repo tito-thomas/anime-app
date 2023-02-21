73b01de44ffd0c406da8b9c284490b862eb7862e
Oluwatito Thomas <thomasoo@cardiff.ac.uk>
2023-02-21 07:08:08 +0000

    added some notes

--- recommendation.py
+++ recommendation.py
@@ -14,3 +14,19 @@ users.head()
 #    return int(years)
 
 #age = uf["birth_date"].apply(to_age)
+
+##ONE-HOT ENCODE GENRES
+
+# Encode the genre field as a list of genres and one-hot encode it
+#genre_encoded = df['genre'].str.split(', ')
+#genre_encoded = genre_encoded.apply(lambda x: pd.Series(x))
+#genre_encoded = pd.get_dummies(genre_encoded.stack()).sum(level=0)
+#genre_df = pd.concat([df['anime_id'], genre_encoded], axis=1)
+#df = pd.merge(df, genre_df, on='anime_id')
+
+
+#LABEL ENCODING OF OTHER CATEGORIES
+#creates dictionary for you
+#from sklearn.preprocessing import LabelEncoder
+#encoder = LabelEncoder()
+#df['type_encoded'] = encoder.fit_transform(df['type'])
