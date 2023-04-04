c98999a4e202d54defcc10ad46f03ebabd11dc4b
Oluwatito Thomas <thomasoo@cardiff.ac.uk>
2023-04-04 04:59:23 +0100

    correct website

---
+++ anime-site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="anime_site.MvcApplication" Language="C#" %>
--- anime-site/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace anime_site.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
